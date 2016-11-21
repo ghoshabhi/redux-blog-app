ed6897dd792da2c3f81f3481b11d08f149f8d54a
ghoshabhi <abhighosh18@gmail.com>
2016-11-20 16:06:28 -0800

    Feat: Add Delete post functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_SINGLE_POST = 'FETCH_SINGLE_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const root_url = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=Ajsdno2fm$mkadlaksd';
@@ -28,6 +29,14 @@ export function fetchSinglePost(postId) {
   const request = axios.get(`${root_url}/posts/${postId}${API_KEY}`);
   return {
     type: FETCH_SINGLE_POST,
+    payload: request
+  }
+}
+
+export function deletePost(postId) {
+  const request = axios.delete(`${root_url}/posts/${postId}${API_KEY}`);
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
--- src/components/single_post.js
+++ src/components/single_post.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchSinglePost } from '../actions/index';
+import { fetchSinglePost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class SinglePost extends Component {
 
   componentWillMount() {
     this.props.fetchSinglePost(this.props.params.id);
+  }
+
+  onDeletePost() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        this.context.router.push('/');
+      })
+      .catch(err => {
+        console.error("Error occured on delete");
+      });
   }
 
   render() {
@@ -19,6 +29,12 @@ class SinglePost extends Component {
 
       <div className="container">
         <Link to='/'>Home</Link>
+        <button
+          className="btn btn-danger pull-xs-right marginTop"
+          onClick = {this.onDeletePost.bind(this)}
+          >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -27,10 +43,20 @@ class SinglePost extends Component {
   }
 };
 
+SinglePost.contextTypes = {
+  router: PropTypes.object.isRequired,
+};
+
 function mapStateToProps(state) {
   return {
     post: state.posts.post
   }
 }
 
-export default connect(mapStateToProps, { fetchSinglePost })(SinglePost);
+export default connect(
+  mapStateToProps,
+  {
+    fetchSinglePost,
+    deletePost
+  }
+  )(SinglePost);
